81958194bda9e6b957ac21c5238ccd55c92c5bdf
Yesid Medina <andres.medina@viliv.com.co>
2022-06-07 08:16:00 -0500

    back usuarios

---
+++ index.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+const { app } = require('./lib/infrastructure/webserver/server')
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
